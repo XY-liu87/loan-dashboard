305dc480194a57e5a5416237aff196caca2ab44a
XY-liu87 <1185874461@qq.com>
2026-06-12 17:08:38 +0800

    debug login

--- 3-上传网站.bat
+++ 3-上传网站.bat
@@ -5,9 +5,9 @@ set GIT=D:\Git\cmd\git.exe
 if not exist "%GIT%" set GIT=git
 
 copy /Y "%~dp0dashboard_v2.html" "%~dp0index.html" >nul
-"%GIT%" add index.html dashboard_v2.html data_v2_enc.js name_map.json .gitignore 3-upload.bat 2-Generate.bat
+"%GIT%" add -A
 "%GIT%" commit -m "update"
 "%GIT%" push origin master
 
-echo Done. https://xy-liu87.github.io/loan-dashboard/
+echo [OK] https://xy-liu87.github.io/loan-dashboard/
 pause
